9f9b4a767b9edc0b91ef0e23e8a12b44f93f191f
Mihkel Kiil <kiil.mihkel@gmail.com>
2024-05-26 11:43:03 +0300

    support for multiple segments

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const calculateMileage = (start, end) => Math.abs(start-end);
+let totalDistance = 0;
 
 const setDistance = () => {
-    let startMileage = document.getElementById("start").value;
-    let endMileage = document.getElementById("end").value;
-
-    let distance = calculateMileage(startMileage, endMileage);
-    let template = `Distance: ${distance}`
-    document.getElementById("distance").innerHTML = template;
-š}
+    let segments = Array.from(document.getElementsByClassName("segment"));
+    totalDistance = 0;
+    segments.forEach(
+        segment => totalDistance += calculateMileage(segment.getElementsByTagName("input")[0].value, segment.getElementsByTagName("input")[1].value)
+    );
+    document.getElementById("distance").innerText = `Distance: ${totalDistance}`;
+}
+const addSegment = () => {
+    
+    let segments = document.getElementsByClassName("segment");
+    let newSegment = segments[0].cloneNode(true);
+    newSegment.getElementsByTagName("input")[0].id = `start-${segments.length+1}`;
+    newSegment.getElementsByTagName("input")[1].id = `end-${segments.length+1}`;
+    newSegment.getElementsByTagName("h2")[0].innerText = `Segment ${segments.length+1}`
+    document.getElementById("segments").appendChild(newSegment);
+}
 window.onload = setDistance;
